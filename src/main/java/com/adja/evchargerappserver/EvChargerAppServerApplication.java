e343835ad8cb9e3cc491e7d70a4189a56457a65d
Talpos Norbert <norberttalpos@gmail.com>
2021-10-03 15:13:34 +0200

    Added Location, swagger

--- src/main/java/com/adja/evchargerappserver/EvChargerAppServerApplication.java
+++ src/main/java/com/adja/evchargerappserver/EvChargerAppServerApplication.java
@@ -3,9 +3,11 @@ package com.adja.evchargerappserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableSwagger2
 public class EvChargerAppServerApplication {
 
 	public static void main(String[] args) {
