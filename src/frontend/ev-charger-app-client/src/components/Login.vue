5e07963e04e4485472b1a5d99416a93bed779385
Talpos Norbert <norberttalpos@gmail.com>
2021-10-20 22:31:45 +0200

    kis css+snackbar

--- src/frontend/ev-charger-app-client/src/components/Login.vue
+++ src/frontend/ev-charger-app-client/src/components/Login.vue
@@ -1,51 +1,66 @@
 <template>
-    <div class="home">
-        <template>
-            <v-card class="mx-auto my-12" max-width="374">
-                <v-card-title class="justify-center">LOGIN</v-card-title>
-                <v-card-text>
-                    <v-container>
-                        <v-row>
-                            <v-col cols="12">
-                                <v-text-field
-                                    autofocus
-                                    v-model="username"
-                                    label="Username"
-                                    outlined
-                                    name="username"
-                                ></v-text-field>
-                            </v-col>
-                        </v-row>
-                        <v-row>
-                            <v-col cols="12">
-                                <v-text-field
-                                    autofocus
-                                    v-model="password"
-                                    :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-                                    label="Password"
-                                    outlined
-                                    :type="show ? 'text' : 'password'"
-                                    @click:append="show = !show"
-                                    name="password"
-                                ></v-text-field>
-                            </v-col>
-                        </v-row>
-                    </v-container>
-                </v-card-text>
-                <v-card-actions class="justify-center">
-                    <v-btn
-                        color="primary"
-                        elevation="2"
-                        raised
-                        rounded
-                        text
-                        @click="login"
-                    >
-                        SIGN IN
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </template>
+    <div class="home" style="position: absolute; height: 100%; width: 100%; background-color: #6abd80;">
+        <v-card class="mx-auto" max-width="400" height="400" style="margin-top: 150px;">
+            <v-card-title class="justify-center">LOGIN</v-card-title>
+            <v-card-text>
+                <v-container>
+                    <v-row>
+                        <v-col cols="12">
+                            <v-text-field
+                                v-model="username"
+                                label="Username"
+                                outlined
+                                name="username"
+                                persistent-placeholder
+                            ></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="12">
+                            <v-text-field
+                                v-model="password"
+                                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                                label="Password"
+                                outlined
+                                :type="show ? 'text' : 'password'"
+                                @click:append="show = !show"
+                                name="password"
+                                persistent-placeholder
+                            ></v-text-field>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-card-text>
+            <v-card-actions class="justify-center">
+                <v-btn
+                    color="primary"
+                    elevation="2"
+                    raised
+                    rounded
+                    text
+                    @click="login"
+                    style="background-color: green;"
+                    width="100px"
+                    height="40px"
+                >
+                    <span style="color: white">SIGN IN</span>
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+        <v-snackbar v-model="loginSnackbar" :timeout="2000" top
+                    height="70px" width="450px" color="#7d181c">
+            <span style="font-size: 16px;">Please enter your username and password</span>
+            <template #action="{ attrs }">
+                <v-btn
+                    color="blue"
+                    text
+                    v-bind="attrs"
+                    @click="loginSnackbar = false"
+                >
+                    Close
+                </v-btn>
+            </template>
+        </v-snackbar>
     </div>
 </template>
 <script>
@@ -58,7 +73,8 @@ export default {
         return {
             username:"",
             password:"",
-            show:false
+            show: false,
+            loginSnackbar: false,
         }
     },
     name: 'login',
@@ -79,7 +95,7 @@ export default {
                 }
             }
             catch(error) {
-                console.log(error);
+                this.loginSnackbar = true;
             }
         }
     },
