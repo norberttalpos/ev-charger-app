c295fe609b58f7d9f3709e9284005232a624e0a6
Talpos Norbert <norberttalpos@gmail.com>
2021-11-26 11:04:18 +0100

    trying to fix email service unsatisfied dependency in docker deployment

---
+++ src/main/java/com/adja/evchargerappserver/email/EmailConfig.java
@@ -0,0 +1,15 @@
+package com.adja.evchargerappserver.email;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class EmailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+}
